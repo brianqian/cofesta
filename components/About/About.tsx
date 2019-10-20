3377b3cd064564b0198675badef259e5b0052381
BQ <qian.brian@gmail.com>
2019-10-20 16:20:37 -0400

    reworded about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -14,12 +14,15 @@ function About() {
     <Container>
       <div></div>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ducimus hic, impedit
-        recusandae accusamus asperiores veritatis ullam, sed, architecto officiis maxime error nihil
-        magni unde molestiae voluptates repudiandae nisi vero. Tempora, corporis, molestias beatae,
-        assumenda dolor quibusdam suscipit fugiat iure veniam eveniet culpa eaque vitae similique
-        delectus consequatur aliquam maxime?
+        Welcome to Cofesta. Come in and grab a drink, whether it be coffee or a glass of wine. At
+        Cofesta, we pride ourselves in our high quality ingredients. Our dishes are made with
+        ingredients fresh from the local farmer's market and never frozen. Our coffee is from{' '}
+        <a href="https://www.devocion.com" target="_blank">
+          Devoción
+        </a>{' '}
+        where beans are delivered from the farms in Columbia to us in less than 30 days.
       </p>
+      <p></p>
     </Container>
   );
 }
